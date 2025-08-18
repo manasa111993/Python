524b7cb588670b34221a4bb4479aa98a52f26ad8
manasa <manasapreethi7@gmail.com>
2025-08-18 11:48:40 +0530

    data file

--- automation/WebAutomation/Heroku_Login/Data/data_file.py
+++ automation/WebAutomation/Heroku_Login/Data/data_file.py
@@ -1,10 +1,7 @@
-
 herokuData = {
     "url": "https://the-internet.herokuapp.com/login",
     "username": "tomsmith",
     "password": "SuperSecretPassword!",
     "wrong_user": "wronguser",
     "wrong_password": "wrongpassword",
-
-
 }
