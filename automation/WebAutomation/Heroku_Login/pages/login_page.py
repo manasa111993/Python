ff509c1f2c7a158698331ac1f4999f4b7f126f38
manasa <manasapreethi7@gmail.com>
2025-08-18 13:34:46 +0530

    login

--- automation/WebAutomation/Heroku_Login/pages/login_page.py
+++ automation/WebAutomation/Heroku_Login/pages/login_page.py
@@ -1,20 +1,9 @@
-from selenium.webdriver.common.by import By
-
 from Heroku_Login.locators.locators import LoginLocators
 from Heroku_Login.pages.base_page import BasePage
-
-
-#from automation.WebAutomation.Heroku_Login.locators.locators import LoginLocators
-#from automation.WebAutomation.Heroku_Login.pages.base_page import BasePage
-
 
 class HerokuLoginPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
-        #self.username_field = (By.ID, "username")
-        #self.password_field = (By.ID, "password")
-        #self.login_button = (By.CSS_SELECTOR, "button[type='submit']")
-        #self.flash_message = (By.ID, "flash")
 
     def open(self,url):
         self.driver.get(url)
@@ -30,6 +19,3 @@ class HerokuLoginPage(BasePage):
 
     def get_flash_message(self):
         return self.get_text(LoginLocators.flash_message)
-
-
-
